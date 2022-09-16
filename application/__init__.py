4d8650bb3123de20b67812467f646092381ba3a9
Mylo19984 <milos.trpkovic@gmail.com>
2022-09-16 10:48:20 +0200

    changes in html files

--- application/__init__.py
+++ application/__init__.py
@@ -2,11 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 import psycopg2
+import configparser
+import os.path
+
+path = ("/Users/mylo/PycharmProjects/simpleDAGAirF/config.ini")
+
+print(path)
+
+config_obj = configparser.ConfigParser()
+config_obj.read(path)
+db_param = config_obj["postgresql"]
+aws_user = config_obj["aws"]
+db_user = db_param['user']
+db_pass = db_param['password']
+db_host = db_param['host']
+db_name = db_param['db']
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = F'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}'
+    #F'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}'
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
--- application/routes.py
+++ application/routes.py
@@ -1,16 +1,15 @@
-import json
-
 from application import app
 from flask import render_template
 from application.form import UserInputForm
 from application.models import FplPlayerData
 from sqlalchemy import text
 from application import db
+import sql_queries
 
-@app.route('/')
-def index():
-    entries = FplPlayerData.query.order_by(FplPlayerData.form.desc()).limit(9).all()
-    return render_template('index.html', entries=entries)
+#@app.route('/')
+#def index():
+#    entries = FplPlayerData.query.order_by(FplPlayerData.form.desc()).limit(9).all()
+#    return render_template('index.html', entries=entries)
 
 @app.route('/layout')
 def layout():
@@ -21,15 +20,26 @@ def add():
     form = UserInputForm()
     return render_template('add.html', title='Addd', form=form)
 
-@app.route('/dashboard')
+@app.route('/')
 def dashboard():
-    sql = text('select name, surname, form from mylo.player_general pg order by form desc limit 9')
-    result = db.engine.execute(sql).fetchall()
+    result = db.engine.execute(sql_queries.basic_ply_data).fetchall()
+    sql_total_cost = text(sql_queries.value_per_points)
+    result_total_cost = db.engine.execute(sql_total_cost).fetchall()
+    sql_general_data_overview = text(sql_queries.detailed_ply_data_per_week)
+
+    result_main_graph = db.engine.execute(sql_general_data_overview).fetchall()
+
     fpl_data = [float(row['form']) for row in result]
-    fpl_data_label = [row['name'] + ' ' + row['surname']  for row in result]
+    fpl_data_label = [row['name'] + ' ' + row['surname'] for row in result]
+    fpl_data_points = [float(row['total_points']) for row in result_total_cost]
+    fpl_data_points_name = [row['name'] + ' ' + row['surname'] for row in result_total_cost]
+    fpl_data_points_value = [float(row['point_value']) for row in result_total_cost]
+    fpl_data_main_table = [row for row in result_main_graph]
     #db.dispose()
 
-    return render_template('dashboard.html', title='Dash', fpl_data = fpl_data, fpl_data_name = fpl_data_label)
+    return render_template('dashboard.html', title='Dash', fpl_data = fpl_data, fpl_data_name = fpl_data_label,
+                           fpl_data_points=fpl_data_points, fpl_data_points_name=fpl_data_points_name, fpl_data_points_value=fpl_data_points_value,
+                           fpl_data_main_table=fpl_data_main_table)
 
 
 
